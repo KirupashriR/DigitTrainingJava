a1e603c0dda85101554e43841cfd177716bd0ccc
KirupashriR <139847522+KirupashriR@users.noreply.github.com>
2023-07-18 12:42:23 +0530

    Update TestingCode.java

--- src/main/java/com/digit/testProj/p1/TestingCode.java
+++ src/main/java/com/digit/testProj/p1/TestingCode.java
@@ -3,7 +3,6 @@ package com.digit.testProj.p1;
 public class TestingCode {
 	public static void main(String[] args) {
 		System.out.println("hello world");
-		System.out.println("hello world");
-		System.out.println("hello world");
+		
 	}
 }
